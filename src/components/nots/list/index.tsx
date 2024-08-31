055655b8df0f0c516f885d4ee1ef8777508d4d32
Arda <a.konca@student.utwente.nl>
2024-08-31 21:56:15 +0200

    latests

--- src/components/nots/list/index.tsx
+++ src/components/nots/list/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Table,
@@ -7,7 +7,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 import {
   Card,
@@ -15,30 +15,46 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 type Props = {
-  notes: {
-    id: string,
-    authorName: string,
-    authorNote: string
-  }
-}
+  notes:
+    | {
+        status: number;
+        message: string;
+        data: {
+          id: string;
+          authorName: string;
+          authorNote: string;
+        }[];
+      }
+    | undefined;
+};
 
 function NotsList({ notes }: Props) {
-
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [allData, setAllData] = useState(notes?.data);
+  const [rowData, setRowData] = useState<{
+    id: string;
+    authorName: string;
+    authorNote: string;
+  }>({
+    id: "",
+    authorName: "",
+    authorNote: "",
+  });
+
+  if (!allData) return <div>Loading</div>;
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Note List</CardTitle>
-        <CardDescription>
-          Yazar ve notlarini listeler.
-        </CardDescription>
+        <CardTitle>Note List </CardTitle>
+        <CardDescription>Yazar ve notlarini listeler.</CardDescription>
       </CardHeader>
       <CardContent className="max-h-[500px] overflow-y-auto"></CardContent>
       <Table>
@@ -46,17 +62,25 @@ function NotsList({ notes }: Props) {
           <TableRow>
             <TableHead className="w-[100px]">Author Name</TableHead>
             <TableHead>Author Note</TableHead>
+            <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-          </TableRow>
+          {allData?.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell className="font-medium">{item.authorName}</TableCell>
+              <TableCell>{item.authorNote}</TableCell>
+              <TableCell>
+                <Button size={"sm"} onClick={() => {
+                  setRowData(item)
+                }}>Edit</Button>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
-    </Card >
-  )
+    </Card>
+  );
 }
 
 export default NotsList;
